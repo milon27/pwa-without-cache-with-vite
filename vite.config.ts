321336a717022e65d13771ea83334694fa923d26
milon27 <mdjahidulislammilon@gmail.com>
2022-11-14 17:03:18 +0600

    build new commit

--- vite.config.ts
+++ vite.config.ts
@@ -7,27 +7,31 @@ export default defineConfig({
   plugins: [react(), VitePWA({
     filename: "m-service-worker.js",
     selfDestroying: true,
-    // injectRegister: 'auto',
-    manifest: {
-      name: "my pwa",
-      short_name: "a short name",
-      description: "a pwa web app",
-      theme_color: "#1a1a1a",
-      background_color: "#1a1a1a",
-      categories: ["test"],
-      icons: [
-        {
-          src: 'logo.png',
-          sizes: '192x192',
-          type: 'image/png'
-        },
-        {
-          src: 'logo-512.png',
-          sizes: '512x512',
-          type: 'image/png'
-        }
-      ]
-    },
-    registerType: 'autoUpdate'
+    manifest: false,
+    registerType: "autoUpdate"
+
+    // selfDestroying: true,
+    // // injectRegister: 'auto',
+    // manifest: {
+    //   name: "my pwa",
+    //   short_name: "a short name",
+    //   description: "a pwa web app",
+    //   theme_color: "#1a1a1a",
+    //   background_color: "#1a1a1a",
+    //   categories: ["test"],
+    //   icons: [
+    //     {
+    //       src: 'logo.png',
+    //       sizes: '192x192',
+    //       type: 'image/png'
+    //     },
+    //     {
+    //       src: 'logo-512.png',
+    //       sizes: '512x512',
+    //       type: 'image/png'
+    //     }
+    //   ]
+    // },
+    // registerType: 'autoUpdate'
   })]
 })
